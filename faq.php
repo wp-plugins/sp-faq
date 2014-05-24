37d756ff14c2628d947e9999e4502be7c9ddeb7f
anoopranawat <anoopranawat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-24 11:05:46 +0000

    added 2.2
    git-svn-id: https://plugins.svn.wordpress.org/sp-faq/tags/2.1@920313 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ faq.php
@@ -0,0 +1,151 @@
+<?php
+/*
+Plugin Name: SP FAQ
+Plugin URL: http://sptechnolab.com
+Description: A simple FAQ plugin
+Version: 2.1
+Author: SP Technolab
+Author URI: http://sptechnolab.com
+Contributors: SP Technolab
+*/
+/*
+ * Register CPT sp_faq
+ *
+ */
+function sp_faq_setup_post_types() {
+
+	$festivals_labels =  apply_filters( 'sp_faq_labels', array(
+		'name'                => 'FAQs',
+		'singular_name'       => 'FAQ',
+		'add_new'             => __('Add New', 'sp_faq'),
+		'add_new_item'        => __('Add New FAQ', 'sp_faq'),
+		'edit_item'           => __('Edit FAQ', 'sp_faq'),
+		'new_item'            => __('New FAQ', 'sp_faq'),
+		'all_items'           => __('All FAQ', 'sp_faq'),
+		'view_item'           => __('View FAQ', 'sp_faq'),
+		'search_items'        => __('Search FAQ', 'sp_faq'),
+		'not_found'           => __('No FAQ found', 'sp_faq'),
+		'not_found_in_trash'  => __('No FAQ found in Trash', 'sp_faq'),
+		'parent_item_colon'   => '',
+		'menu_name'           => __('FAQ', 'sp_faq'),
+		'exclude_from_search' => true
+	) );
+
+
+	$faq_args = array(
+		'labels' 			=> $festivals_labels,
+		'public' 			=> true,
+		'publicly_queryable'=> true,
+		'show_ui' 			=> true,
+		'show_in_menu' 		=> true,
+		'query_var' 		=> true,
+		'capability_type' 	=> 'post',
+		'has_archive' 		=> true,
+		'hierarchical' 		=> false,
+		'supports' => array('title','editor','thumbnail','excerpt'),
+		'taxonomies' => array('category', 'post_tag')
+	);
+	register_post_type( 'sp_faq', apply_filters( 'sp_faq_post_type_args', $faq_args ) );
+
+}
+
+add_action('init', 'sp_faq_setup_post_types');
+/*
+ * Add [sp_faq limit="-1"] shortcode
+ *
+ */
+function sp_faq_shortcode( $atts, $content = null ) {
+	
+	extract(shortcode_atts(array(
+		"limit" => ''
+	), $atts));
+	
+	// Define limit
+	if( $limit ) { 
+		$posts_per_page = $limit; 
+	} else {
+		$posts_per_page = '-1';
+	}
+	
+	ob_start();
+
+	// Create the Query
+	$post_type 		= 'sp_faq';
+	$orderby 		= 'post_date';
+	$order 			= 'DESC';
+				
+	$query = new WP_Query( array ( 
+								'post_type'      => $post_type,
+								'posts_per_page' => $posts_per_page,
+								'orderby'        => $orderby, 
+								'order'          => $order,
+								'no_found_rows'  => 1
+								) 
+						);
+	
+	//Get post type count
+	$post_count = $query->post_count;
+	$i = 1;
+	
+	// Displays Custom post info
+	
+	
+	
+	if( $post_count > 0) :
+	?>
+	
+	 <div id="accordion">
+	<?php
+		// Loop 
+		while ($query->have_posts()) : $query->the_post();
+		?>
+		
+		
+		 <h3> <?php the_title(); ?></h3>
+		 <div>
+<p>  <?php
+                  if ( function_exists('has_post_thumbnail') && has_post_thumbnail() ) {
+                    the_post_thumbnail('thumbnail'); 
+                  }
+                  ?>
+				  <?php echo get_the_content(); ?></p></div>
+		
+	
+		<?php
+		$i++;
+		endwhile; ?>
+		</div>
+		
+<?php	endif;
+	
+	// Reset query to prevent conflicts
+	wp_reset_query();
+	
+	?>
+	
+	<?php
+	
+	return ob_get_clean();
+
+}
+
+	add_shortcode("sp_faq", "sp_faq_shortcode");
+
+	wp_register_style( 'accordioncss', plugin_dir_url( __FILE__ ) . 'css/jqueryuicss.css' );
+	wp_register_script( 'accordionjs', plugin_dir_url( __FILE__ ) . 'js/jqueryuijs.js', array( 'jquery' ) );	
+
+	wp_enqueue_style( 'accordioncss' );
+	wp_enqueue_script( 'accordionjs' );
+	function myfaqscript() {
+	?>
+	<script type="text/javascript">
+	
+	 //jQuery( "#accordion" ).accordion(); 
+	 
+	   jQuery( "#accordion" ).accordion({
+heightStyle: "content"
+});
+	</script>
+	<?php
+	}
+add_action('wp_footer', 'myfaqscript'); 
---
+++ readme.txt
@@ -0,0 +1,92 @@
+=== SP responsive wp-faq plugin ===
+Contributors: SP Technolab, anoopranawat 
+Tags: faq, faq list, faq plugin, faqs, jquery ui, jquery ui accordion,  faq with accordion, custom post type with accordion, frequently asked questions, wordpress, wordpress faq, WordPress Plugin, shortcodes
+Requires at least: 3.1
+Tested up to: 3.7
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A quick, easy way to add an responsive FAQs page. You can use this plugin as a jquery ui accordion.
+
+== Description ==
+Many CMS site needs a FAQs section. SP faqs plugin  allows you add, manage and display FAQ on your wordpress website.
+
+This faqs plugin add a FAQs page in your wordpress website with  jquery ui accordion.
+
+The faq plugin adds a "FAQ" tab to your admin menu, which allows you to enter FAQ Title and FAQ Description items just as you would regular posts.
+
+we have also used faq  jquery ui accordion function so that user can show/hide FAQ titles.
+
+To use this FAQ plugin just create a new page and add this FAQ short code <code>[sp_faq limit="-1"]</code>
+
+= Features include: =
+* Just create a FAQs page and add short code <code>[sp_faq limit="-1"]</code>
+* jquery ui accordion
+* Add thumb image for FAQ
+* Easy to configure FAQ page
+* Smooth FAQ Accordion effect
+* Smoothly integrates this paq plugin into any theme
+* CSS and JS file for FAQ custmization
+* Search Engine Friendly URLs
+
+SP FAQ allows you to provide a well-designed and informative FAQ section, which can  decrease the amount of user inquiries on various issues.
+With the help of given CSS file for this FAQ plugin you can desgin this FAQ plugin as per your layout.
+
+== Installation ==
+
+1. Upload the 'sp-faq' folder to the '/wp-content/plugins/' directory.
+2. Activate the sp-faq list plugin through the 'Plugins' menu in WordPress.
+3. Add a new page and add this short code <code>[sp_faq limit="-1"]</code>
+
+
+== Frequently Asked Questions ==
+
+= What templates FAQs are available? =
+
+There is one templates named 'faq.php' which work like same as defult POST TYPE in wordpress.
+
+You can also change the css as per your layout
+
+= Are there shortcodes for FAQs items? =
+
+Yes, Add a new faq page and add this short code <code>[sp_faq limit="-1"]</code>
+
+
+
+== Screenshots ==
+
+1. all Faqs
+2. Add new Faq
+3. preview faq
+
+== Changelog ==
+
+= 2.1 =
+* Added jquery UI
+* Adds custom post type
+* Adds FAQs
+* Smooth accordion effect
+
+= 2.0 =
+* Adds custom post type
+* Adds FAQs
+* New css and css file
+* Smooth accordion effect
+
+= 1.0 =
+* Initial release
+* Adds custom post type
+* Adds FAQs
+
+
+== Upgrade Notice ==
+
+= 2.1 =
+added new jquery ui
+
+= 2.0 =
+added new css and js file
+
+= 1.0 =
+Initial release
